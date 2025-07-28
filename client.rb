77960f8c82375f2ecd6da8bdfbfb22205626eeea
Rakesh Bhatt <bhattrakesh073@gmail.com>
2025-07-29 00:56:16 +0545

    feat: initialize the values dynamically based on key and value from json record
    - also set attribute readers

--- client.rb
+++ client.rb
@@ -4,17 +4,17 @@
 # initializes the Client data record and make additional changes to client records
 #
 class Client
-  attr_reader :id, :full_name, :email
-
   #
   # initializes the client records
   #
   # @param [Hash] attrs - individual client record
   #
   def initialize(attrs)
-    @id = attrs['id']
-    @full_name = attrs['full_name']
-    @email = attrs['email']
+    attrs.each do |key, value|
+      instance_variable_set("@#{key}", value)
+    end
+
+    self.class.attr_reader(*attrs.keys.map(&:to_sym))
   end
 
   #
